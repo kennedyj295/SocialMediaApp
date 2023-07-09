8fb7a426b59bd5d164c049a71e49d6cbe31f7ac9
kennedyj295 <115056868+kennedyj295@users.noreply.github.com>
2023-07-09 18:20:58 -0500

    finished photo uploading and added reactive registration form

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,10 +19,8 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user)
-        {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+        if(user) {
+          this.setCurrentUser(user);
         }
       })
     )
@@ -32,14 +30,14 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+            this.setCurrentUser(user);
         }
       })
     )
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
